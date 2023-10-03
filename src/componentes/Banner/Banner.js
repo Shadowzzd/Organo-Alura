e883196de8655f1a3d9d35126b1e5b9423eeb905
Guilherme Bartholo <137460791+Shadowzzd@users.noreply.github.com>
2023-10-03 18:44:12 -0300

    Primeiro projeto utilizando React, organograma com funções de adicionar e deletar colaboradores da alura

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return (
+        <header className="banner">
+        <img src="/imagens/Banner.svg"     alt=" O banner principal da página do Organo"/>
+        </header>
+    )
+
+}
+
+export default Banner
